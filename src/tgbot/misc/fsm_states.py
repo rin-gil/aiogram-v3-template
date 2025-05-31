911604065ba236bb193acb6d7b326ed2315732e0
rin-gil <91410075+rin-gil@users.noreply.github.com>
2025-05-31 18:24:03 +0300

    change module description

--- src/tgbot/misc/fsm_states.py
+++ src/tgbot/misc/fsm_states.py
@@ -1,6 +1,1 @@
-"""
-Describes the state for the FSM (Final State Machine)
-
-Read more at: https://docs.aiogram.dev/en/latest/dispatcher/fsm.html?highlight=fsm
-Example: https://docs.aiogram.dev/en/latest/examples/finite_state_machine_example.html
-"""
+"""Describes the state for the FSM (Final State Machine)."""
